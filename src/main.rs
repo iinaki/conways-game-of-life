949b3b5cbf72c707017e78c75cac3d06c0a027e9
iñaki llorens <inakillorens@Laptop-de-inaki.local>
2024-08-20 16:16:27 -0300

    add macroquad basic shapes to test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, BLACK);
+
+        next_frame().await
+    }
 }
