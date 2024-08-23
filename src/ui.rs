2804c4d6a0f7dbcc2e5b1f4eb02c27642507d79d
iñaki llorens <inakillorens@Laptop-de-inaki.local>
2024-08-23 12:55:29 -0300

    Replace Sleep for FPS to control game time

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use crate::game::Game;
 use ::rand::Rng;
 use macroquad::prelude::*;
 
-use std::{thread::sleep, time::Duration};
+use std::time::Instant;
 
 const SQUARES: i16 = 32;
 const TEXT_SIZE: f32 = 25.;
@@ -38,24 +38,20 @@ const DEFAULT_SEED: [(i32, i32); 8] = [
 ///
 async fn display_ui(
     game: &mut Game,
-    game_running: &mut bool,
     game_paused: &mut bool,
     edit_mode: &mut bool,
     generations_passed: &mut i32,
+    acc: &mut f32,
+    fps: &mut f32,
+    prev_t: &mut Instant,
 ) {
-    let (offset_x, offset_y, sq_size) = render_screen(game, generations_passed);
+    let elapsed = prev_t.elapsed().as_secs_f32();
+    *prev_t = Instant::now();
+    *acc += elapsed;
 
-    if *game_paused {
-        if *edit_mode {
-            run_edit_mode(game, &offset_x, &offset_y, &sq_size);
-        } else {
-            run_paused_mode();
-        }
-    } else {
-        run_playing_mode(game, generations_passed);
-    }
+    handle_commands(game, game_paused, edit_mode, fps);
 
-    handle_commands(game, game_running, game_paused, edit_mode);
+    render_screen(game, generations_passed, game_paused, edit_mode, fps, acc);
 
     next_frame().await;
 }
@@ -67,7 +63,14 @@ async fn display_ui(
 /// - `game`: The current game state.
 /// - `generations_passed`: The number of generations that have passed in the game.
 ///
-fn render_screen(game: &Game, generations_passed: &i32) -> (f32, f32, f32) {
+fn render_screen(
+    game: &mut Game,
+    generations_passed: &mut i32,
+    game_paused: &mut bool,
+    edit_mode: &mut bool,
+    fps: &f32,
+    acc: &mut f32,
+) {
     clear_background(LIGHTGRAY);
     let screen_width = screen_width();
     let screen_height = screen_height();
@@ -120,12 +123,50 @@ fn render_screen(game: &Game, generations_passed: &i32) -> (f32, f32, f32) {
         BLACK,
     );
 
-    (offset_x, offset_y, sq_size)
+    let text = format!("FPS: {}", 1. / fps);
+    draw_text(
+        &text,
+        screen_width * GENERATIONS_POS_X,
+        screen_height * GENERATIONS_POS_Y + 15.,
+        TEXT_SIZE,
+        BLACK,
+    );
+
+    render_mode(
+        game,
+        (offset_x, offset_y, sq_size),
+        game_paused,
+        edit_mode,
+        generations_passed,
+        fps,
+        acc,
+    );
+}
+
+fn render_mode(
+    game: &mut Game,
+    screen_params: (f32, f32, f32),
+    game_paused: &mut bool,
+    edit_mode: &mut bool,
+    generations_passed: &mut i32,
+    fps: &f32,
+    acc: &mut f32,
+) {
+    if *game_paused {
+        if *edit_mode {
+            run_edit_mode(game, screen_params);
+        } else {
+            run_paused_mode();
+        }
+    } else {
+        run_playing_mode(game, generations_passed, fps, acc);
+    }
 }
 
 /// Handles the edit mode where users can add or remove cells.
-fn run_edit_mode(game: &mut Game, offset_x: &f32, offset_y: &f32, sq_size: &f32) {
+fn run_edit_mode(game: &mut Game, screen_params: (f32, f32, f32)) {
     show_edit_hud();
+    let (offset_x, offset_y, sq_size) = screen_params;
 
     if is_mouse_button_pressed(MouseButton::Left) {
         let (mouse_x, mouse_y) = mouse_position();
@@ -219,12 +260,14 @@ fn show_pause_hud() {
 }
 
 /// Runs the game in playing mode, where time passes and the game is updated.
-fn run_playing_mode(game: &mut Game, generations_passed: &mut i32) {
-    sleep(Duration::from_secs(1));
-    game.update_game();
+fn run_playing_mode(game: &mut Game, generations_passed: &mut i32, fps: &f32, acc: &mut f32) {
+    if *acc >= *fps {
+        game.update_game();
+        *generations_passed += 1;
+        *acc -= fps;
+    }
 
     show_playing_hud();
-    *generations_passed += 1;
 }
 
 /// Displays the HUD for playing mode, showing instructions for the user.
@@ -260,19 +303,17 @@ fn show_playing_hud() {
         TEXT_SIZE,
         BLACK,
     );
+    draw_text(
+        "Use top and down arrows to change the speed of the game",
+        screen_width * TEXT_POS_X,
+        screen_height * TEXT_POS_Y + 45.,
+        TEXT_SIZE,
+        BLACK,
+    );
 }
 
 /// Handles users commands.
-fn handle_commands(
-    game: &mut Game,
-    game_running: &mut bool,
-    game_paused: &mut bool,
-    edit_mode: &mut bool,
-) {
-    if is_key_pressed(KeyCode::X) {
-        *game_running = !*game_running;
-    }
-
+fn handle_commands(game: &mut Game, game_paused: &mut bool, edit_mode: &mut bool, fps: &mut f32) {
     if is_key_pressed(KeyCode::Space) {
         *game_paused = !*game_paused;
     }
@@ -288,6 +329,14 @@ fn handle_commands(
 
     if is_key_pressed(KeyCode::R) {
         restart_game_with_random_seed(game);
+    }
+
+    if is_key_pressed(KeyCode::Up) {
+        *fps = (*fps * 0.9).max(1.0 / 144.0);
+    }
+
+    if is_key_pressed(KeyCode::Down) {
+        *fps = (*fps * 1.1).min(1.0);
     }
 }
 
@@ -317,18 +366,26 @@ fn restart_game_with_random_seed(game: &mut Game) {
 pub async fn run_ui() {
     let mut game = Game::new_with_seed(DEFAULT_SEED.to_vec());
 
-    let mut game_running = true;
     let mut game_paused = false;
     let mut edit_mode = false;
     let mut generations_passed = 0;
 
-    while game_running {
+    let mut fps: f32 = 1.0 / 5.0;
+    let mut acc = 0.0_f32;
+    let mut prev_t = Instant::now();
+
+    // let current_time = Instant::now();
+    // println!("Current time: {:?}", current_time.as_secs_f64());
+
+    while !is_key_pressed(KeyCode::X) {
         display_ui(
             &mut game,
-            &mut game_running,
             &mut game_paused,
             &mut edit_mode,
             &mut generations_passed,
+            &mut acc,
+            &mut fps,
+            &mut prev_t,
         )
         .await;
     }
